ac63b570ff83074c8fe37b0ca86f94eae6403e19
unknown <unknown@example.com>
2021-03-23 13:53:54 +0800

    :tada: for the first time to submit

---
+++ components/TabBar/TabBar.js
@@ -0,0 +1,38 @@
+// components/TabBar/TabBar.js
+
+Component({
+  options: {
+    addGlobalClass: true
+  },
+  /**
+   * pageCur：当前页码
+   * classNmae: 额外添加的属性
+   * list: 每页的信息
+   */
+  properties: {
+    pageCur: {
+      type: Number, // 属性
+      value: 0 // 默认类型
+    },
+    className: String,
+    list: {
+      type: Array,
+      value: []
+    }
+  },
+  /**
+   * tabChange: tab点击触发函数
+   */
+  methods: {
+    tabChange(e) {
+      const {index} = e.currentTarget.dataset
+      if (index === this.data.pageCur) {
+        return
+      }
+      this.setData({
+        pageCur: index
+      })
+      this.triggerEvent('change', {index, item: this.data.list[index]})
+    }
+  }
+})
