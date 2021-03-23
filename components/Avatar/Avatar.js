eb373f65ebfebdcdb176ccebb79e8902373af07d
unknown <unknown@example.com>
2021-03-23 19:13:55 +0800

    :new: create a Avatar component

---
+++ components/Avatar/Avatar.js
@@ -0,0 +1,28 @@
+// components/Avatar/Avatar.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    size:{ // 头像大小
+      type:String,
+      default:'normal'
+    },
+    imageSrc:String, // 背景图路径
+    isRound:Boolean // 边缘是园的
+  },  
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
