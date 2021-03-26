67840e165cd8d30fac2d7d736545a2bcaaa5b273
unknown <unknown@example.com>
2021-03-26 19:52:21 +0800

    :new: create the card component

---
+++ components/Card/Card.js
@@ -0,0 +1,26 @@
+// components/Card/Card.js
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    isCard:{
+      type:Boolean,
+      value:false
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+  }
+})
